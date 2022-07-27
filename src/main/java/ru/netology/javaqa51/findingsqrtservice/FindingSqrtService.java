2ceb13a51f9d9b01aa1b18d715e6550bcdec25be
Elnar Kilmukhametov <soulmiker@gmail.com>
2022-07-27 12:54:53 +0300

    Third commit

--- src/main/java/ru/netology/javaqa51/findingsqrtservice/FindingSqrtService.java
+++ src/main/java/ru/netology/javaqa51/findingsqrtservice/FindingSqrtService.java
@@ -1,10 +1,10 @@
 package ru.netology.javaqa51.findingsqrtservice;
 
 public class FindingSqrtService {
-    public static int calculate(int roundA, int roundB) {
+    public static int calculate(int borderA, int borderB) {
         int numberSqrt = 0;
         for (int i = 10; i < 100; i++) {
-            if (i * i >= roundA & i * i <= roundB) {
+            if (i * i >= borderA & i * i <= borderB) {
                 numberSqrt++;
             }
         }
--- src/test/java/ru/netology/javaqa51/findingsqrtservice/FindingSqrtTest.java
+++ src/test/java/ru/netology/javaqa51/findingsqrtservice/FindingSqrtTest.java
@@ -1,18 +1,39 @@
 package ru.netology.javaqa51.findingsqrtservice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class FindingSqrtTest {
-    @ParameterizedTest
-    @CsvFileSource(files="src/test/resources/InitialParameters.csv")
-    public void testFindingSqrt(int expected, int roundA, int roundB) {
+    @Test
+    public void threeSqrWithinTheBorders200_300() {
 
         FindingSqrtTest service = new FindingSqrtTest();
 
-        int actual = FindingSqrtService.calculate(roundA, roundB);
+        int actual = FindingSqrtService.calculate(200, 300);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void zeroSqrWithinTheBorders20_30() {
+
+        FindingSqrtTest service = new FindingSqrtTest();
+
+        int actual = FindingSqrtService.calculate(20, 30);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void oneSqrWithinTheBorders100_100() {
+
+        FindingSqrtTest service = new FindingSqrtTest();
+
+        int actual = FindingSqrtService.calculate(100, 100);
+        int expected = 1;
 
         Assertions.assertEquals(expected, actual);
     }
